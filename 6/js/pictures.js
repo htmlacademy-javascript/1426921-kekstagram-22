559092d4f9e6d4f5ebc4ee9605a25045ddf4384c
Keks <keks@htmlacademy.ru>
2021-02-18 22:03:39 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/pictures.js
@@ -0,0 +1,32 @@
+import photos from './data.js';
+import showBigPicture from './big-picture.js';
+
+const pictures = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const renderPhoto = (picture) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('img').src = picture.url;
+  pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
+  pictureElement.querySelector('.picture__likes').textContent = picture.likes;
+
+  pictureElement.addEventListener('click', (evt) => {
+    evt.preventDefault();
+
+    showBigPicture(picture);
+  });
+
+  return pictureElement;
+};
+
+const renderPhotos = () => {
+  const fragment = document.createDocumentFragment();
+
+  photos.forEach((item) => {
+    fragment.appendChild(renderPhoto(item));
+  });
+
+  pictures.appendChild(fragment);
+};
+
+renderPhotos();
